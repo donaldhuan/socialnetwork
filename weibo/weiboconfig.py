413a0565991fa3b04e6a186a429b1909633fc6f5
donaldhjw <hjwdonald@gmail.com>
2013-01-15 21:04:56 +0800

    modify the weibo configuration file

--- weibo/weiboconfig.py
+++
@@ -1,6 +0,0 @@
-APP_KEY= '3552598539'
-APP_SECRET = '08bfca1eb74c4d89720af38f72b98a7e'
-CALLBACK_URI = 'http://www.donaldway.com'
-#private account info
-ACCOUNT = 'hjwdonald@gmail.com'
-PASSWORD = 'HJW6385514hjw@'
